c5ad01196395d01db1f7b33c656b26542ce74f47
hamza chaouni <chaounihamza19@gmail.com>
2022-12-19 20:06:32 +0000

    Commit calculatrice

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,196 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+  <div class="calc-body">
+    <div class="calc-screen">
+      <div class="calc-typed">{{current || '0'}}<span class="blink-me">_</span></div>
+    </div>
+    <div class="calc-button-row">
+      <div @click="clear" class="button c">C</div>
+      <div @click="divide" class="button l">≠</div>
+      <div @click="percent" class="button l">%</div>
+      <div @click="sign" class="button l">/</div>
+    </div>
+    <div class="calc-button-row">
+      <div @click="append('7')" class="button">7</div>
+      <div @click="append('8')" class="button">8</div>
+      <div @click="append('9')" class="button">9</div>
+      <div @click="times" class="button l">x</div>
+    </div>
+    <div class="calc-button-row">
+      <div @click="append('4')" class="button">4</div>
+      <div @click="append('5')" class="button">5</div>
+      <div @click="append('6')" class="button">6</div>
+      <div @click="minus" class="button l">−</div>
+    </div>
+    <div class="calc-button-row">
+      <div @click="append('1')" class="button">1</div>
+      <div @click="append('2')" class="button">2</div>
+      <div @click="append('3')" class="button">3</div>
+      <div @click="add" class="button l">+</div>
+    </div>
+    <div class="calc-button-row">
+      <div @click="dot" class="button">.</div>
+      <div @click="append('0')" class="button">0</div>
+      <div class="button">V3</div>
+      <div @click="equal" class="button l">=</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      current: '',
+      operatorClicked: false,
+      operator: null,
+      previous: null,
+    }
+  },
+  methods: {
+    clear() {
+      this.current = ''
+    },
+    sign() {
+      this.current = this.current.charAt(0) === '-' ? 
+        this.current.slice(1) : `-${this.current}`;
+    },
+    percent() {
+      this.current = `${parseFloat(this.current) / 100}`;
+    },
+    append(number) {
+      if (this.operatorClicked) {
+        this.current = '';
+        this.operatorClicked = false;
+      }
+      this.current = `${this.current}${number}`;
+    },
+    dot() {
+      if (this.current.indexOf('.') === -1 && this.current != '') {
+        this.append('.');
+      }
+    },
+    setPrevious() {
+      this.previous = this.current;
+      this.operatorClicked = true;
+    },
+    divide() {
+      this.operator = (a, b) => a / b;
+      this.setPrevious()
+    },
+    times() {
+      this.operator = (a, b) => a * b;
+      this.setPrevious()
+    },
+    minus() {
+      this.operator = (a, b) => a - b;
+      this.setPrevious()
+    },
+    add() {
+      this.operator = (a, b) => a + b;
+      this.setPrevious()
+    },
+    equal() {
+      this.current = `${this.operator(
+        parseFloat(this.current), 
+        parseFloat(this.previous)
+      )}`;
+      this.previous = null;
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+* {
+  box-sizing: border-box;
+  font-family: 'Roboto', sans-serif;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+body {
+  background: #EAEBEC;
 }
-li {
+
+.container {
+  width: 400px;
+  margin: auto;
+}
+
+.calc-body {
+  width: 275px;
+  margin: auto;
+  min-height: 400px;
+  border: solid 1px #3A4655;
+  box-shadow: 0 8px 50px -7px black;
+}
+
+.calc-screen {
+  background: #3A4655;
+  width: 100%;
+  height: 115px;
+  padding: 20px;
+}
+
+.calc-operation {
+  text-align: right;
+  color: #727B86;
+  font-size: 21px;
+  padding-bottom: 10px;
+  border-bottom: dotted 1px;
+}
+
+.calc-typed {
+  margin-top: 20px;
+  font-size: 45px;
+  text-align: right;
+  color: #fff;
+}
+
+.calc-button-row {
+  width: 100%;
+  background: #3C4857;
+}
+
+.button {
+  width: 25%;
+  background: #425062;
+  color: #fff;
+  padding: 20px;
   display: inline-block;
-  margin: 0 10px;
+  font-size: 25px;
+  text-align: center;
+  vertical-align: middle;
+  margin-right: -4px;
+  border-right: solid 2px #3C4857;
+  border-bottom: solid 2px #3C4857;
+  transition: all 0.2s ease-in-out;
 }
-a {
-  color: #42b983;
+
+.button.l {
+  color: #AEB3BA;
+  background: #404D5E;
+}
+
+.button.c {
+  color: #D95D4E;
+  background: #404D5E;
+}
+
+.button:hover {
+  background: #E0B612;
+  transform: rotate(5deg);
+}
+
+.button.c:hover,
+.button.l:hover {
+  background: #E0B612;
+  color: #fff;
+}
+
+.blink-me {
+  color: #E0B612;
 }
 </style>
